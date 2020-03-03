61720e534ee1b2c84115c968aeafb1e11104e67a
ErickZCortes <erickccortes@gmail.com>
2020-03-03 00:26:42 -0600

    users

--- resources/views/mikvo/dashboard/modules/user/updateuser.blade.php
+++ resources/views/mikvo/dashboard/modules/user/updateuser.blade.php
@@ -29,48 +29,59 @@
 </header>
 @if ( !empty ( $user->id) )
 <h1 class="h3 mb-2 text-gray-800">Perfil del usuario</h1>
-<div class="container">
             <div class="card shadow mb-4">
                 <div class="card-body">
                 <form method="POST" action="{{ route('/dashboard/user/update',$user->id) }}" role="form" enctype="multipart/form-data">
  
+                    
                     <input type="hidden" name="_method" value="PUT">
                     <input type="hidden" name="_token" value="{{ csrf_token() }}">
                             <div class="form-row">
-                                <div class="form-group col-md-6">
+                                <div class="form-group col-md-4">
                                     <label for="fullname_user">Nombre completo</label>
                                     <input 
                                     id="fullname_user"
                                     type="text" 
-                                    class="form-control" 
+                                    class="form-control text-capitalize" 
                                     name="fullname_user"
                                     value="{{ $user->fullname_user }}"
                                     required
                                     >
                                 </div>
-                                <div class="form-group col-md-6">
+                                <div class="form-group col-md-4">
                                     <label for="user_name">Usuario</label>
                                     <input 
                                     id="user_name"
                                     type="text" 
-                                    class="form-control" 
+                                    class="form-control text-capitalize" 
                                     name="user_name"
                                     value="{{ $user->user_name }}"
                                     required
                                     >
                                 </div>
-                                <div class="form-group col-md-6">
+                                <div class="form-group col-md-4">
                                     <label for="telephone_user">Teléfono</label>
                                     <input 
                                     id="telephone_user"
-                                    type="text" 
+                                    type="tel" 
                                     class="form-control" 
                                     name="telephone_user"
                                     value="{{ $user->telephone_user }}"
                                     required
                                     >
                                 </div>
-                                <div class="form-group col-md-6">
+                                <div class="form-group col-md-4">
+                                    <label for="email_user">Email</label>
+                                    <input 
+                                    id="email_user"
+                                    type="email" 
+                                    class="form-control" 
+                                    name="email_user"
+                                    value="{{ $user->email_user }}"
+                                    required
+                                    >
+                                </div>
+                                <div class="form-group col-md-4">
                                     <label for="password_user">Contraseña</label>
                                     <input 
                                     id="password_user"
@@ -83,21 +94,35 @@
                                         Debe tener entre 8 y 15 caracteres de largo.
                                     </small>
                                 </div>
+                        
                                 <div class="form-group col-md-12">
-                                <label for="img_user" class="negrita">Selecciona una imagen:</label> 
-                                <hr>            
-                                <div>
-                                <input name="img_user" type="file" id="img_user">   
+                                    <h5 class="mb-2 text-gray-500">Imagen de perfil</h5>
                                 </div>
-                            <div class="center">
-                                <button type="submit" class="btn btn-primary">Actualizar cuenta</button>
-                            </div>
-                        </form>
-                    
-                </div>
+                                    <div class="col-md-6" style=" text-align: center;
+                                    padding-top: 50PX;">
+                                        <label for="img_user" class="negrita">Selecciona una imagen:</label>
+                                        <span class="btn btn btn-dark btn-file">Seleccionar archivo<input type="file" name="img_user" type="file" id="img_user">
+                                    </div>
+                                @if ( !empty ( $user->img_user) )
+                                <div class="col-md-6">
+                                    <span>Imagen Actual:</span>
+                                        <img src="../../../../uploads/{{$user->img_user}}" width="200" class="img-fluid">
+                                        <br>
+                                        <div class="col-md-4 center">
+                                    </span>
+                                </div>
+                        </div>
+                  @else
+                    Aún no se ha cargado una imagen para este producto
+                  @endif                         
+                    <div class=" col-md-12 center">
+                        <br>
+                        <button type="submit" class="btn btn-primary">Actualizar cuenta</button>
+                    </div>
+                </form>     
+        </div>
+    </div>
 
-            </div>
-</div>
 @endif
 </html>
 @endsection
--- resources/views/mikvo/dashboard/modules/user/userprofile.blade.php
+++ resources/views/mikvo/dashboard/modules/user/userprofile.blade.php
@@ -28,42 +28,44 @@
     <link rel="stylesheet"type="text/css" href="{{URL::asset('css/app.css')}}">
 </header>
 
-<h1 class="h3 mb-2 text-gray-800">Perfil del usuario</h1>
-<div class="container">
-            <div class="text-center card shadow mb-4 mx-auto">
-                <div class="card-body">
-                @if (( $user->img_user) == "null")
-                <img class="rounded-circle"
-                        src="../../../../uploads/user-default.png" alt="" width="200"
-                        height="200"> 
-              @else
-              <img class="rounded-circle"
-                        src="../../../../uploads/{{$user->img_user}}" alt="" width="200"
-                        height="200">
-                 
-              @endif     
-                <hr>
-                    <h4 class="mb-0">{{$user->user_name}}</h4>
-                    <span class="text-muted d-block mb-2">User Name</span>
-                    <hr>
-                    <div class="row">
-                        <div class="col-md-6">
-                        {{$user->fullname_user}}
-                            <span class="text-muted d-block mb-2">Nombre</span>
-                        </div>
-                        
-                        <div class="col-md-6">
-                        {{$user->telephone_user}}
-                            <span class="text-muted d-block mb-2">Teléfono</span>
+    <h1 class="h3 mb-2 text-gray-800">Perfil del usuario</h1>
+        <div class="text-center card shadow mb-4 mx-auto">
+            <div class="card-body">
+                <div class="row">
+                    <div class="col-md-6">
+                        @if (( $user->img_user) == "null")
+                            <img class="rounded mx-auto d-block" src="../../../../uploads/user-default.png" alt="" width="250" height="220"> 
+                        @else
+                            <img class="rounded mx-auto d-block" src="../../../../uploads/{{$user->img_user}}" alt="" width="250" height="220">
+                        @endif   
+                        <hr>
+                        <h4 class="mb-0 text-uppercase" >{{$user->user_name}}</h4>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="form-group">
+                            <div class=" col-md-12">
+                               <div class="col-md-3"><span class="text-muted lead d-block mb-2">Nombre:</span></div>
+                               <div class="col-md-9 text-capitalize"><h5>{{$user->fullname_user}}</h5></div>
+                            </div>
+                            <div class="col-md-12">
+                                <div class="col-md-3"><span class="text-muted lead d-block mb-2">Teléfono:</span></div>
+                                <div class="col-md-9"><h5>{{$user->telephone_user}}</h5></div>
+                            </div>
+                            <div class="col-md-12">
+                                <div class="col-md-3"><span class="text-muted lead d-block mb-2">Email:</span></div>
+                                <div class="col-md-9"><h5>{{$user->email_user}}</h5></div>
+                            </div>
+                            <div class="col-md-12">
+                                <div class="col-md-3"><span class="text-muted lead d-block mb-2">Registrado:</span></div>
+                                <div class="col-md-9"><h5>{{$user->created_at}}</h5></div>
+                            </div>
                         </div>
                     </div>
-                    <div class="row">
-                        <div class="col-md-12">
+                    <div class="col-md-12">
                         <a href="{{ route('/dashboard/user/edit', $user->id) }}" class="btn btn-success mt-4 ml-3">Editar usuario</a>
-                        </div>
                     </div>
                 </div>
             </div>
-</div>
+        </div>
 </html>
 @endsection
